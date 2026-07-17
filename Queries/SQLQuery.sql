2359335f459c4b041cbc621f4046deeb2dd12f80
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-07-17 17:16:24 -0300

    Update SQLQuery.sql
    corrigindo erros

--- Queries/SQLQuery.sql
+++ Queries/SQLQuery.sql
@@ -504,7 +504,7 @@ CREATE TABLE dw.dim_tempo (
    ============================================================ */
   CREATE TABLE dw.dim_pet(
     id_pet INT IDENTITY(1,1) PRIMARY KEY,
-    cod_pet INT,
+    cod_pet INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     especie VARCHAR(100) NOT NULL,
     porte VARCHAR(100) NOT NULL,
@@ -517,26 +517,29 @@ CREATE TABLE dw.dim_tempo (
 /* ============================================================
    7.7 DIMENSÃO TURNO
    
+   A dimensão tempo é carregada separadamente.
+   
+   Turno:
+   - Manhã
+   - Terça
+   - Noite
+   ============================================================ */
+  CREATE TABLE dw.dim_turno(
+    id_turno INT IDENTITY(1,1) PRIMARY KEY,
+    turno VARCHAR(5) NOT NULL
+  );
+  /* ============================================================
+   7.8 DIMENSÃO QUADRO CLINICO
+   
    Implementação da estratégia SCD Tipo 2.
 
    Etapas:
    1. atualiza os registros existentes;
    2. insere os novos registros.
    ============================================================ */
-  CREATE TABLE dw.dim_turno(
-    id_turno INT IDENTITY(1,1) PRIMARY KEY,
-    turno VARCHAR(5)
-  );
-  /* ============================================================
-   7.8 DIMENSÃO QUADRO CLINICO
-   
-   - Manhã
-   - Terça
-   - Noite
-   ============================================================ */
   CREATE TABLE dw.dim_quadro_clinico(
-    id_turno INT IDENTITY(1,1) PRIMARY KEY,
-    cod_quadro_clinico INT,
+    id_quadro_clinico INT IDENTITY(1,1) PRIMARY KEY,
+    cod_quadro_clinico INT NOT NULL,
     situacao_inicial VARCHAR(500) NOT NULL, 
     situacao_final VARCHAR(500),
     data_inicio DATE NOT NULL,
